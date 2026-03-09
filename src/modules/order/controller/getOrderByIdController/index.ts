0ad0f9218052640a1a76bb4c5b05d802c80d8883
Anthony-Santana11 <anthonysantana110306@gmail.com>
2026-03-09 00:24:51 -0300

    fix: correct destructuring of orderId from request parameters in GetOrderByIdController

--- src/modules/order/controller/getOrderByIdController/index.ts
+++ src/modules/order/controller/getOrderByIdController/index.ts
@@ -5,7 +5,7 @@ export class GetOrderByIdController {
   constructor(private getOrderByIdUseCase: GetOrderByIdUseCase) {}
 
   async handle(req: Request, res: Response): Promise<Response> {
-    const { orderId } = req.params;
+    const orderId = req.params.orderId as string;
 
     const order = await this.getOrderByIdUseCase.execute(orderId);
     return res.status(200).json(order);
